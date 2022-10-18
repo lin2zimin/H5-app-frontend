a393a11a1af4a1d67233d7ecc3948cf47731b6e6
Zimin Lin <lin@ZimindeMacBook-Pro.local>
2022-10-18 14:17:28 +0800

    fix:修改config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
             host:'120.77.155.183',
             ref:'origin/master',
             repo:'https://github.com/lin2zimin/H5-app-frontend.git',
-            path:'/frontend-code',
+            path:'/workspace/H5-app-frontend',
             'post-deploy':'git reset --hard && git checkout master && git pull && npm i --production=false && npm run build:release && pm2 startOrReload ecosystem.config.js',
             env:{
                 NODE_ENV:'production'
